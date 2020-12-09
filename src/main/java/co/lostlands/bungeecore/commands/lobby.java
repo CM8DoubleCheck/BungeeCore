d0045534e7b3c3a167eb283672d924a21d3008ab
CM8DoubleCheck <68802586+CM8DoubleCheck@users.noreply.github.com>
2020-12-08 22:07:26 -0600

    Added catch for when the lobby server does not exist

--- src/main/java/co/lostlands/bungeecore/commands/lobby.java
+++ src/main/java/co/lostlands/bungeecore/commands/lobby.java
@@ -5,8 +5,11 @@ import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
+
+import java.util.logging.Level;
 
 public class lobby extends Command {
 
@@ -25,8 +28,14 @@ public class lobby extends Command {
                 //Already connected to the lobby server
                 p.sendMessage(new ComponentBuilder("You are already in the lobby").color(ChatColor.RED).create());
             } else {
-                p.sendMessage(new ComponentBuilder("Sending you to the lobby!").color(ChatColor.GREEN).create());
-                p.connect(ProxyServer.getInstance().getServerInfo(plugin.getConfig().getString("lobby")));
+                ServerInfo target = ProxyServer.getInstance().getServerInfo(plugin.getConfig().getString("lobby"));
+                if (target != null) {
+                    p.sendMessage(new ComponentBuilder("Sending you to the lobby!").color(ChatColor.GREEN).create());
+                    p.connect(target);
+                } else {
+                    p.sendMessage(new ComponentBuilder("Failed to send you to the lobby.").color(ChatColor.RED).create());
+                    plugin.getLogger().log(Level.WARNING, ChatColor.RED + "Failed to route player "+p.getName()+" to the lobby: Server does not exist.");
+                }
             }
 
         }
